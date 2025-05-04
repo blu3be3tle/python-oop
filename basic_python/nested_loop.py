87365dc9c1ae13c9dd7100cfd91791f3424f1de9
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-05-04 23:08:15 +0600

    first commit

---
+++ basic_python/nested_loop.py
@@ -0,0 +1,12 @@
+salahs = ['fazr', 'zuhr', 'asr', 'magrib', 'isha', 'witr']
+rakahs = [4, 12, 4, 5, 6, 3]
+
+prayer_comb = []
+
+for salah in salahs:
+    print('Salah: ', salah)
+    for rakah in rakahs:
+        print(salah, rakah)
+        prayer_comb.append([salah, rakah])
+    
+    print(prayer_comb)
